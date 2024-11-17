c989105c55b779da773c4299f0ed9083862d2285
d-sava <dimitrijevicsava8@gmail.com>
2024-11-17 18:17:36 +0100

    modified:   src/CommandlineInterface.java

--- src/CommandlineInterface.java
+++ src/CommandlineInterface.java
@@ -4,7 +4,30 @@ package src;
 import java.util.Scanner;
 
 public class CommandlineInterface {
+    private final Calculator calculator;
+
+    public CommandlineInterface(){
+        this.calculator = new Calculator();
+    }
+
     public void startInteractiveMode(){
-        //code
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Scientific calculator (type 'exit' to quit): ");
+
+        while(true){
+            System.out.print("> ");
+            String input = scanner.nextLine();
+
+            if(input.equalsIgnoreCase("exit"))
+                break;
+
+            try{
+                double result = calculator.evaluate(input);//TODO create evalute method
+                System.out.println("Result: " +result);
+
+            } catch(CalculatorException e){
+                System.out.println("Error: " +e.getMessage());//TODO create getMessage method
+            }
+        }
     }
 }
